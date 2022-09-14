93c48c251a071225168a862710b76874524c1094
Oscar Morales <oscar.s.morales.z@usp.br>
2022-09-14 15:28:58 -0500

    Modify file

--- routes/tracks.js
+++ routes/tracks.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const { validatorCreateItem } = require("../validators/tracks");
 const { getItems, createItem } = require("../controllers/tracks");
 
 //TODO http://localhost/tracks GET, POST, DELETE, PUT
 
 router.get("/", getItems);
-router.post("/", createItem);
+router.post("/", validatorCreateItem, createItem);
 
 module.exports = router
