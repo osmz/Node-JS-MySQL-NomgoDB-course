55fece817054b96151a1bfb09f070ac189e15d88
Oscar Morales <oscar.s.morales.z@usp.br>
2022-09-17 16:53:32 -0500

    Modify file

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const { matchedData } = require("express-validator");
+const { encrypt, compare } = require("../utils/handlePassword");
 const router = express.Router();
 const { validatorRegister, validatorLogin } = require("../validators/auth");
 
-router.post("/register", validatorRegister, (req, res) => {
+router.post("/register", validatorRegister, async (req, res) => {
     req = matchedData(req);
-    res.send({ data:req })
+    const password = await encrypt(req.password)
+    const body = {...req, password}
+    res.send({ data:body })
 })
 
 module.exports = router
