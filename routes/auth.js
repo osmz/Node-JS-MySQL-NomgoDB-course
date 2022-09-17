083ed06f6fdcd30527941e73f0f5b49d031d4f1c
Oscar Morales <oscar.s.morales.z@usp.br>
2022-09-17 17:42:22 -0500

    Modify file

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { matchedData } = require("express-validator");
+const { tokenSign } = require("../utils/handleJwt");
 const { encrypt, compare } = require("../utils/handlePassword");
 const { usersModel } = require("../models");
 const router = express.Router();
@@ -9,8 +10,13 @@ router.post("/register", validatorRegister, async (req, res) => {
     req = matchedData(req);
     const password = await encrypt(req.password);
     const body = {...req, password};
-    const data =  await usersModel.create(body);
-    data.set("password", undefined, { strict: false });
+    const dataUser =  await usersModel.create(body);
+    dataUser.set("password", undefined, { strict: false });
+
+    const data = {
+        token: await tokenSign(dataUser),
+        user:dataUser
+    }
     res.send({ data });
 });
 
---
+++ utils/handleJwt.js
@@ -0,0 +1,36 @@
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = process.env.JWT_SECRET;
+
+/**
+ * Debes de pasar el objeto del usuario
+ * @param {*} user 
+ */
+const tokenSign = async (user) => {
+    const sign = jwt.sign(
+        {
+            _id: user._id,
+            role: user.role
+        },
+        JWT_SECRET,
+        {
+            expiresIn:"2h",
+        }
+    );
+    return sign
+};
+
+/**
+ * Debes de psar el token de session el Jwt
+ * @param {*} tokenJwt 
+ * @returns 
+ */
+const verifyToken = async (tokenJwt)=> {
+    try{
+        return jwt.verify(tokenJwt, JWT_SECRET);
+
+    }catch(e){
+        return null
+    }
+};
+
+module.exports = { tokenSign, verifyToken };
