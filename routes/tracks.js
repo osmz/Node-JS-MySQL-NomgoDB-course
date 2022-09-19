92501d5270dc5fb9c9c419ac49f74580f6077fb4
Oscar Morales <oscar.s.morales.z@usp.br>
2022-09-19 18:49:54 -0500

    Modify file

--- routes/tracks.js
+++ routes/tracks.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const authMiddleware = require("../middleware/session");
+const checkRol = require("../middleware/rol");
 const { validatorCreateItem, validatorGetItem } = require("../validators/tracks");
 const { getItems, getItem, createItem, updateItem, deleteItem } = require("../controllers/tracks");
 
 //TODO http://localhost/tracks GET, POST, DELETE, PUT
 
 router.get("/", authMiddleware, getItems);
-router.get("/:id", validatorGetItem, getItem);
-router.post("/", validatorCreateItem, createItem);
-router.put("/:id", validatorGetItem, validatorCreateItem, updateItem);
-router.delete("/:id", validatorGetItem, deleteItem);
+router.get("/:id", authMiddleware, validatorGetItem, getItem);
+router.post("/", authMiddleware, checkRol(["admin"]), validatorCreateItem, createItem);
+router.put("/:id", authMiddleware, validatorGetItem, validatorCreateItem, updateItem);
+router.delete("/:id", authMiddleware, validatorGetItem, deleteItem);
 
 module.exports = router
