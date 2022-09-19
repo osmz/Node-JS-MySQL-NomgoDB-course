46cc676fcba6ec2242380b1419d6a7198e903dd2
Oscar Morales <oscar.s.morales.z@usp.br>
2022-09-19 18:25:29 -0500

    Created middleware/session.js file

---
+++ middleware/session.js
@@ -0,0 +1,30 @@
+const { handleHttpError } = require("../utils/handleError");
+const { verifyToken } = require("../utils/handleJwt");
+const { usersModel } = require("../models");
+
+const authMiddleware = async (req, res, next) => {
+    try{
+        if(!req.headers.authorization){
+            handleHttpError(res, "NEED SESSION", 401);
+            return
+        }
+
+        const token = req.headers.authorization.split(' ').pop();
+        const dataToken = await verifyToken(token);
+
+        if(!dataToken._id){
+            handleHttpError(res, "ERROR ID TOKEN", 401);
+            return
+        }
+
+        const user = await usersModel.findById(dataToken._id)
+        req.user = user
+
+        next()
+
+    }catch(e){
+        handleHttpError(res, "NOT SESSION", 401);
+    }
+};
+
+module.exports = authMiddleware
