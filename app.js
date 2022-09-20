d80cbe747f1808a68bf9bccacfe0f650f251fb18
Oscar Morales <oscar.s.morales.z@usp.br>
2022-09-19 19:11:48 -0500

    Add morgan-body

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
-require("dotenv").config()
-const express = require("express")
-const cors = require("cors")
-const dbConnect = require("./config/mongo")
-const app = express()
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const morganBody = require("morgan-body");
+const dbConnect = require("./config/mongo");
+const app = express();
 
 app.use(cors())
 app.use(express.json())
 app.use(express.static("storage"))
 
+const loggerStream = {
+    write: message => {
+        console.log('Capturando el LOG', message)
+    },
+};
+
+morganBody(app, {
+    noColors:true,
+    stream: loggerStream
+})
+
 const port = process.env.PORT || 3000
+
 
 /**
  * Aqui llamamos a las rutas
